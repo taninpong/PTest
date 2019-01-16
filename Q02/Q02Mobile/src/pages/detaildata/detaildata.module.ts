d94cbe740b005d68ee835386f869136c1ce30fdf
thaninpong <thaninpong@perfenterprise.com>
2019-01-16 12:51:28 +0700

    Implement UI Q02

---
+++ Q02/Q02Mobile/src/pages/detaildata/detaildata.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { DetaildataPage } from './detaildata';
+
+@NgModule({
+  declarations: [
+    DetaildataPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(DetaildataPage),
+  ],
+})
+export class DetaildataPageModule {}
